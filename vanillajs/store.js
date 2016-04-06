4d19e288f83da0d6e976c7d1e354949dd8eed5da
godspeedyoo <godspeedyoo@gmail.com>
2016-04-05 23:46:45 -0700

    Refactors and completes basic store functionality

--- vanillajs/store.js
+++ vanillajs/store.js
@@ -1,39 +1,41 @@
 (function(window) {
   'use strict';
 
+  // PUBLIC
+
   function Store(name, callback) {
     this._dbName = name;
     findOrCreateLocalStorageDb(name);
     // callback.call(this, object)
-  }
-
-  function localStorageExist(name) {
-    return !!localStorage[name];
-  }
-
-  function findOrCreateLocalStorageDb(name) {
-    if (!localStorageExist(name)) {
-      var data = { todos: [] };
-      createLocalStorageDb(name, data);
+    this.config = {
+      onSaveSuccessMessage: 'Todo item successfully saved!'
     }
   }
 
-  function createLocalStorageDb(name, data) {
+  Store.prototype.writeToLocalStorageDb = function (name, data) {
     localStorage[name] = JSON.stringify(data);
   }
 
-  // @params {object} |todoItem| todoItem object
-  // @params {function} |callback| callback to fire after save
-  // @params {integer} |id| ID of todo item
-  Store.prototype.save = function(todoItem, callback, id) {
+  Store.prototype.save = function (todoItem, callback, id) {
     var data = this.loadData();
-    debugger;
+    data.todos.push(todoItem);
+    this.writeToLocalStorageDb(data);
+    callback.call(this, data);
   }
 
   // This can be changed to an ajax request if data will be stored in the backend
   Store.prototype.loadData = function() {
     return JSON.parse(localStorage[this._dbName]);
   }
+
+  // PRIVATE
+  function findOrCreateLocalStorageDb(name) {
+    if (!localStorage[name]) {
+      var data = { todos: [] };
+      writeToLocalStorageDb(name, data);
+    }
+  }
+
   // export to window
   window.app = window.app || {};
   window.app.Store = Store;
@@ -43,4 +45,4 @@
 
 // execution code for testing
 var s = new app.Store('test-db');
-s.save({ title: "do something" }, function() { console.log("Saved!")});
+s.save({ title: "do something" }, function(message) { console.log(message)});
