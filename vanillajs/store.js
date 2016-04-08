21b41f8d530f833fc79f40b5ba7c7018391a0a41
godspeedyoo <godspeedyoo@gmail.com>
2016-04-07 22:40:44 -0700

    Fix js lint error

--- vanillajs/store.js
+++ vanillajs/store.js
@@ -10,11 +10,11 @@
       onSaveSuccessMessage: 'Todo item successfully saved!',
       // define default database structure in localStorage
       defaultDb: { todos: [] }
-    }
+    };
 
     this.writeToLocalStorageDb = function (name, data) {
       localStorage[name] = JSON.stringify(data);
-    }
+    };
 
     this.writeToLocalStorageDb(name);
     // callback.call(this, object)
@@ -25,14 +25,14 @@
     data.todos.push(todoItem);
     this.writeToLocalStorageDb(data);
     callback.call(this, data);
-  }
+  };
 
   // This can be changed to an ajax request if data will be stored in the backend
   Store.prototype.loadData = function() {
     var data = localStorage[this._dbName];
     // localStorage returns "undefined" as string if undefined
     return (data !== 'undefined') ?  data : this.config.defaultDb;
-  }
+  };
 
   // PRIVATE
   function findOrCreateLocalStorageDb(name) {
@@ -49,5 +49,6 @@
 })(window);
 
 // execution code for testing
+/* jshint strict: false*/
 var s = new app.Store('test-db');
-s.save({ title: "do something" }, function(message) { console.log(message)});
+s.save({ title: "do something" }, function(message) { console.log(message);});
